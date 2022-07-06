b5f132916b330c6b1cd4dea141968d39e9dd97e8
weiwait <mail@weiwait.cn>
2022-07-06 10:44:06 +0800

    增加表单必填

--- version.php
+++ version.php
@@ -8,5 +8,11 @@ return [
         '增加文件直传表单组件',
         'create_weiwait_uploads_table.php',
         'create_filesystem_configs_table.php',
+    ],
+    '1.1.1' => [
+        '一些优化'
+    ],
+    '1.1.2' => [
+        '增加表单必填(required)'
     ]
 ];
